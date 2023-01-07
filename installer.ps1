95911bb22b39515ddc53da3293c0f50c9ea43838
ERIK R ANDERSON <primex001@gmail.com>
2023-01-07 13:37:59 -0500

    Initial commit

---
+++ installer.ps1
@@ -0,0 +1,20 @@
+choco install vscode - y
+choco install notepadplusplus - y
+choco install brave -y 
+choco install git -y
+choco install putty.install -y
+choco install python38 -y
+choco install 1password -y
+choco install rufus -y
+choco install icue -y
+choco install winscp -y
+choco install spotify -y 
+choco install rancher-desktop -y
+choco install steam -y 
+choco install onenote -y 
+choco install office365business -y 
+choco install discord -y 
+choco install drivereasyfree -y 
+choco install virtualbox -y
+choco install wireshark -y 
+choco install vmwarevsphereclient -y 
